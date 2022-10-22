0806a216f2004d28445f19a994eb96baed6fcdee
YvanLanglois <42yvan@gmail.com>
2022-10-22 09:58:07 +0200

    deplacement

--- Assets/Scripts/deplacement.cs
+++ Assets/Scripts/deplacement.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class deplacement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    Vector2 movement;
+
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        
+        movement = new Vector2();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movement * moveSpeed;
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        movement = ctx.ReadValue<Vector2>();
     }
 }
